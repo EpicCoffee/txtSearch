192e3937cd3d924ca7393a6dc2885caeb96d97da
EpicCoffee <ludvig.ojl@gmail.com>
2020-03-28 14:31:09 +0100

    Created structure of the project and added SearchStrings method.

---
+++ src/model/TextFile.java
@@ -0,0 +1,33 @@
+package model;
+
+public class TextFile
+{
+	private String fileName;
+	private String[] contentWords;
+
+	public TextFile(String fileName, String[] contentWords)
+	{
+		this.fileName = fileName;
+		this.contentWords = contentWords;
+	}
+
+	public String getFileName()
+	{
+		return fileName;
+	}
+
+	public void setFileName(String fileName)
+	{
+		this.fileName = fileName;
+	}
+
+	public String[] getContentWords()
+	{
+		return contentWords;
+	}
+
+	public void setContentWords(String[] contentWords)
+	{
+		this.contentWords = contentWords;
+	}
+}
---
+++ src/test/controller/DocumentHandlerTest.java
@@ -0,0 +1,15 @@
+package controller;
+
+import model.TextFile;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DocumentHandlerTest
+{
+	@Test
+	void searchStrings()
+	{
+		TextFile myTextFile = new TextFile("Name", new String[]{"hej", "hopp", "katt", "mus", "Janne", "malle", "Kalle", "Ulle", "paka", "makka", "katt"});
+		assertEquals(4, DocumentHandler.searchStrings(myTextFile, "hej", "katt", "Janne").size(), "I do not find the words.");
+	}
+}
