e15540c1f632f87f788baa5e9630cc36bce19692
EpicCoffee <ludvig.ojl@gmail.com>
2020-04-07 14:55:46 +0200

    added allLoadedFielsPane to view.

--- src/controller/FileSearchController.java
+++ src/controller/FileSearchController.java
@@ -1,10 +1,18 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import model.Session;
+import model.TextFile;
 
 public class FileSearchController
 {
@@ -41,11 +49,30 @@ public class FileSearchController
 
 	public void initializeAll()
 	{
-
+		initializeAllLoadedFilesPane();
 	}
 
 	private void initializeAllLoadedFilesPane()
 	{
+		VBox vBox = new VBox();
+		for (TextFile textfile : Session.getSession().getLoadedTextFiles())
+		{
+			Label titleLabel = new Label(textfile.getFileName());
 
+			titleLabel.setOnMouseClicked(event -> {
+				if (Session.getSession().getChoosenDocuments().contains(textfile))
+				{
+					titleLabel.setTextFill(Color.web("#000000"));
+					Session.getSession().removeAtChoosenDocuments(textfile);
+				}
+				else
+				{
+					titleLabel.setTextFill(Color.web("#12b500"));
+					Session.getSession().addToChoosenDocuments(textfile);
+				}
+			});
+			vBox.getChildren().add(titleLabel);
+		}
+		allLoadedFilesPane.getChildren().add(vBox);
 	}
 }
