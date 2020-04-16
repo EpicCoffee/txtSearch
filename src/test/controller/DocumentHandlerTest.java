a05002ee906c60c09d4daa8f3053cdd269db011e
EpicCoffee <ludvig.ojl@gmail.com>
2020-04-16 13:04:03 +0200

    Added comments to unit tests.

--- src/test/controller/DocumentHandlerTest.java
+++ src/test/controller/DocumentHandlerTest.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.Test;
 class DocumentHandlerTest
 {
 
+	/*
+		Checking if alphabeticSort works by having one unsorted array and one sorted.
+	 */
+
 	@Test
 	void alphabeticSort()
 	{
@@ -18,6 +22,10 @@ class DocumentHandlerTest
 		assertArrayEquals(sorted, DocumentHandler.alphabeticSort(unsorted),"Array is not alphabetic");
 	}
 
+	/*
+		Checking if searchTextFileContentWithWords works by creating a text file and expecting it to find words 4 times in it.
+	 */
+
 	@Test
 	void searchTextFileContentWithWords()
 	{
@@ -25,6 +33,10 @@ class DocumentHandlerTest
 		assertEquals(4, DocumentHandler.searchTextFileContentWithWords(myTextFile, "hej", "katt", "Janne").size(), "I do not find the words.");
 	}
 
+	/*
+		Checking if createSortedTextFileRatingsArrayList works by creating an ArrayList with TextFiles and one with the expected TextFileRatings in order.
+		I use the choosenDocuments in the createSortedTextFileRatingsArrayList function and expect it to return a sorted TextFileRatings in same way as the expectedTextFileRatings are.
+ 		*/
 	@Test
     void createSortedTextFileRatingsArrayList()
 	{
@@ -34,22 +46,25 @@ class DocumentHandlerTest
 		chosenDocuments.add(new TextFile("text3", "Janne gillar katter och han själv är en mus, katter, mus, katter"));
 		chosenDocuments.add(new TextFile("text4", "Rebecca har en hund som är vän med en mus som är vän med en mus"));
 
-		ArrayList<TextFileRatings> correctRatings = new ArrayList<>();
-		correctRatings.add(new TextFileRatings(5, chosenDocuments.get(2)));
-		correctRatings.add(new TextFileRatings(2, chosenDocuments.get(3)));
-		correctRatings.add(new TextFileRatings(1, chosenDocuments.get(1)));
-		correctRatings.add(new TextFileRatings(0, chosenDocuments.get(0)));
+		ArrayList<TextFileRatings> expectedTextFileRatings = new ArrayList<>();
+		expectedTextFileRatings.add(new TextFileRatings(5, chosenDocuments.get(2)));
+		expectedTextFileRatings.add(new TextFileRatings(2, chosenDocuments.get(3)));
+		expectedTextFileRatings.add(new TextFileRatings(1, chosenDocuments.get(1)));
+		expectedTextFileRatings.add(new TextFileRatings(0, chosenDocuments.get(0)));
 
 		ArrayList<TextFileRatings> testRating = DocumentHandler.createSortedTextFileRatingsArrayList(chosenDocuments, "katter", "mus");
 
 		for (int i = 0; i < testRating.size(); i++)
 		{
-			assertEquals(correctRatings.get(i).getTextFile(), testRating.get(i).getTextFile(), "One of the documents at rating is not the same");
-			assertEquals(correctRatings.get(i).getRating(), testRating.get(i).getRating(), "One of the rating is the documents are not the same");
+			assertEquals(expectedTextFileRatings.get(i).getTextFile(), testRating.get(i).getTextFile(), "One of the documents at rating is not the same");
+			assertEquals(expectedTextFileRatings.get(i).getRating(), testRating.get(i).getRating(), "One of the rating is the documents are not the same");
 		}
     }
 
-	//Test by having 4 .txt files at C:\txtSearch
+	/*
+		Test by having 4 .txt files at C:\txtSearch
+		Checking if loadAllMyTextFiles() loads all 4 to loadedTextFiles at our Session.
+	 */
 	@Test
 	void loadAllMyTextFiles()
 	{
@@ -57,24 +72,33 @@ class DocumentHandlerTest
 		assertEquals(4, Session.getSession().getLoadedTextFiles().size(), "You do not have 4 txt files in folder C:\\txtSearch");
 	}
 
-	// Make sure Marcus historia2 do not exist when starting test.
+	/*
+		Make sure Marcus historia2 do not exist when starting test.
+		It will first try to create the file Marcus historia2 and return result.
+		It will then edit the same file and return the result of the edit.
+	 */
 	@Test
 	void editOrCreateTextFile()
 	{
-		// first creates the file.
 		assertTrue(DocumentHandler.editOrCreateTextFile("Marcus historia2", "123Banana o puff"));
-		// Edit the created file
+
 		assertTrue(DocumentHandler.editOrCreateTextFile("Marcus historia2", "Marcus the best"));
 	}
 
+	/*
+		Edit or creates the file Marcus hisoria2 to test at this file.
+		Checking if the content of the file is same as what getContentInTextFileAt returns. Trims the result to get rid of extra spaces at end.
+ 	*/
 	@Test
 	void getContentInTextFileAt()
 	{
 		DocumentHandler.editOrCreateTextFile("Marcus historia2", "Marcus the best");
-		System.out.println(DocumentHandler.getContentInTextFileAt(Constants.folderPath + "Marcus historia2.txt"));
 		assertEquals("Marcus the best", DocumentHandler.getContentInTextFileAt(Constants.folderPath + "Marcus historia2.txt").trim(), "The content at Marcus histora2 is wrong");
 	}
 
+	/*
+	Checking if it gives back you only words in a String[] from a sentence in a String.
+ */
 	@Test
 	void getWordsFromString()
 	{
